1b159fd4dfd88d82a3c5953ed904285b565cfd1b
Joblyn <oaikhenahjob@gmail.com>
2020-12-06 01:34:55 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ronzlsdesk/precache-manifest.3b38b019ae9ad630b1a1a40297b2dd83.js"
+  "/ronzlsdesk/precache-manifest.5c0e8de278aa8d4055ec1b40fec37c8d.js"
 );
 
 workbox.clientsClaim();
